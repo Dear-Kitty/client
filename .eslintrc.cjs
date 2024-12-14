ecec031bc4bfa74af0acb020d767937e1ee8220f
gixxla <gigixquisite@gmail.com>
2024-12-15 00:13:38 +0900

    Feat: Initial commit
    - eslint, prettier 세팅
    - package.json, tsconfig.json 작성

---
+++ .eslintrc.cjs
@@ -0,0 +1,50 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'eslint-config-prettier',
+  ],
+  root: true,
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      node: {},
+      typescript: {
+        directory: './src',
+      },
+    },
+    'import/parsers': { '@typescript-eslint/parser': ['.ts', '.tsx'] },
+  },
+  ignorePatterns: ['dist', 'public'],
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'parent', 'sibling', 'index', 'object', 'type'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'newlines-between': 'never',
+      },
+    ],
+    'import/no-unresolved': 'off',
+    'import/export': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-key': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+};
