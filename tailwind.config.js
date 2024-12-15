078651d65b671b93f562fb307d89db005eefe0fb
gixxla <gigixquisite@gmail.com>
2024-12-15 20:51:28 +0900

    Design: Tailwind CSS 및 전역 스타일 설정
    - tailwind.config, index.css 설정
    - 기타 패키지 매니저 및 tsconfig, gitignore 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      bold: ['Paperlogy-7Bold'],
+      semibold: ['Paperlogy-6Semibold'],
+      medium: ['Paperlogy-5Medium'],
+    },
+    extend: {
+      colors: {
+        background: {
+          overlay: 'rgba(0, 0, 0, 0.3)',
+        },
+        button: {
+          primary: '#000000',
+          secondary: '#808080',
+          danger: '#C22D40',
+        },
+        text: {
+          primary: '#000000',
+          secondary: '#2D3748',
+          muted: '#A0AEC0',
+        },
+        border: {
+          light: '#D9D9D9',
+          dark: '#2D3748',
+        },
+        login: {
+          border: '#A9A9A9',
+          text: '#888888',
+        },
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: ['tailwind-scrollbar-hide'],
+};
